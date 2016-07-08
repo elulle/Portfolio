52bbd5cc2d447704ccff8a7c8dfb0f7ebb43feb2
elulle <elulle@outlook.com>
2016-07-08 12:29:42 +0300

    Connected to google sheet and displayed current location

--- js/initial.js
+++ js/initial.js
@@ -1,7 +1,49 @@
 $(document).ready(function(){
 
+	var myLocations = [],
+		myPic = [], 
+		myText = [],
+		myDatabase = [],
+		currentLocation = '';
+
+	loadData = (function() {
+		var url="https://docs.google.com/spreadsheets/d/1OPUMaE5qEN0mXxJ-U4UPVpkaAMh5oAJrbzYE6t5Ir1Y/pub?output=csv";
+		// var url="https://spreadsheets.google.com/feeds/cells/1OPUMaE5qEN0mXxJ-U4UPVpkaAMh5oAJrbzYE6t5Ir1Y/od6/public/values?alt=json-in-script&callback=myCallback";
+		xmlhttp=new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if(xmlhttp.readyState == 4 && xmlhttp.status==200){
+				//get and split csv file
+				myDatabase=xmlhttp.responseText.split("\n");
+
+				myLocations = myDatabase[0].split(",");
+				myPic = myDatabase[1].split(",");
+				myText = myDatabase[2].split(",");
+
+			    console.log(myLocations);
+			    console.log(myPic);
+			    console.log(myText);
+
+			    currentLocation = myLocations[myLocations.length - 1];
+			    currentPic = myPic[myPic.length - 1];
+			    currentText = myText[myText.length - 1];
+
+			    console.log(currentLocation);
+			    console.log(currentPic);
+			    console.log(currentText);
+
+			    document.getElementById("actual-location").innerHTML = "<h1>"+currentLocation+"</h1>";
+			    $('.header').css('backgroundImage', 'url('+currentPic+')');
+			    document.getElementById("actual-text").innerHTML = "<p>"+currentText+"</p>";
+			}
+		};
+	    xmlhttp.open("GET",url,true);
+	    xmlhttp.send(null);
+
+	  })();
+
 	$("#location-check").click(function(){
 	    $("#location-check").hide();
-	    $("#actual-location").fadeIn();
+	    $("#actual-info").fadeIn();
 	});
-  }) 
+
+})
